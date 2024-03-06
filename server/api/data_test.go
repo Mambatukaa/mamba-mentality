dc6a05caf2ac81934f767e4e59bc16490506ffe0
mambatukaa <sbatuka9@gmail.com>
2024-03-06 13:54:20 -0800

    add unit test example

---
+++ server/api/data_test.go
@@ -0,0 +1,17 @@
+package api_test
+
+import (
+	"testing"
+
+	"mamba-mentality.com/api"
+)
+
+
+func TestApiCall(t *testing.T) {
+ _, err := api.GetRate("");
+
+ if(err == nil) {
+	t.Error("Error was not found");
+ }
+
+}
