14f99c61564711d3ddca39bb430cf4fe813f89fa
mambatukaa <sbatuka9@gmail.com>
2024-03-05 22:51:55 -0800

    add go thread example

--- server/main.go
+++ server/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"mamba-mentality.com/api"
 )
 
 func main() {
+	currencies := []string {"BTC", "ETH", "BCH"}
 
-	rate, err := api.GetRate("BTC");
+	var wg sync.WaitGroup;
+	
+	for _, currency := range(currencies) {
+		wg.Add(1);
+
+		go func (currencyCode string) {
+			getCurrencyData(currencyCode);
+			wg.Done();
+		}(currency)
+
+	}
+
+	wg.Wait()
+};
+
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency);
 
 	if(err == nil) {
 		fmt.Printf("The rate for %v is %.2f\n", rate.Currency, rate.Price);
 	};
-};
+}
