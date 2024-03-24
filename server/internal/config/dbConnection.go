73021eb6905bf1d4b38864c4a9489a4f1664cfd6
mambatukaa <sbatuka9@gmail.com>
2024-03-23 23:16:01 -0700

    update connection string

--- server/internal/config/dbConnection.go
+++ server/internal/config/dbConnection.go
@@ -15,18 +15,28 @@ var (
 )
 
 func DbConnection() {
-	DATABASE_URL := os.Getenv("DATABASE_URL")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DATABASE := os.Getenv("DATABASE")
+
+	fmt.Println(DB_HOST)
+	fmt.Println(DB_USER)
+	fmt.Println(DB_PASSWORD)
+	fmt.Println(DATABASE)
 
 	ctx := context.Background()
 
-	if DATABASE_URL == "" {
-		fmt.Fprintf(os.Stderr, "Make sure your DB connection url okay? URL: %v", DATABASE_URL)
+	if DATABASE == "" {
+		fmt.Fprintf(os.Stderr, "Make sure your DB connection url okay? URL: %v", DATABASE)
 		os.Exit(1)
 	}
 
+	var connectionString string = fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=require", DB_HOST, DB_USER, DB_PASSWORD, DATABASE)
+
 	// connect PostreSQL
 	var err error
-	dbpool, err = pgxpool.New(ctx, DATABASE_URL)
+	dbpool, err = pgxpool.New(ctx, connectionString)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
